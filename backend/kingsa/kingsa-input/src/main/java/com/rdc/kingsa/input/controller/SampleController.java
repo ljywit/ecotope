964a8acf79e94dd71b95290fe750b91f472956ae
ljywit <ljywit@163.com>
2018-09-16 01:45:50 +0800

    工程环境搭建-Swagger

--- backend/kingsa/kingsa-input/src/main/java/com/rdc/kingsa/input/controller/SampleController.java
+++ backend/kingsa/kingsa-input/src/main/java/com/rdc/kingsa/input/controller/SampleController.java
@@ -1,4 +1,35 @@
 package com.rdc.kingsa.input.controller;
 
+import com.rdc.kingsa.model.dto.water.quality.WaterQualityFCView;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.apache.log4j.Logger;
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Map;
+
+@Api(tags = "测试Swagger")
+@RestController
+@RequestMapping("/sample")
 public class SampleController {
+
+    private static Logger LOGGER = Logger.getLogger(SampleController.class);
+
+
+    @ApiOperation(value = "测试map大小", httpMethod = "POST", produces = "application/json;charset=utf-8")
+    @RequestMapping(value = "test001",method = RequestMethod.POST, produces = "application/json;charset=utf-8")
+    public int test001(@ApiParam("入参") @NotBlank @RequestBody Map<String,String> inputMap) {
+        return inputMap.size();
+    }
+
+    @ApiOperation(value = "测试格式", httpMethod = "POST", produces = "application/json;charset=utf-8")
+    @RequestMapping(value = "test002",method = RequestMethod.POST, produces = "application/json;charset=utf-8")
+    public String test001(@ApiParam("入参") @RequestBody WaterQualityFCView view) {
+        return view.toString();
+    }
+
+
 }
